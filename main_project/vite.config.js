583d58e177de5caa811bbd6b37e21cb92ce3491a
hsuchiashih <hsuchiashih@gmail.com>
2023-11-02 12:54:02 +0000

    初步設定

---
+++ main_project/vite.config.js
@@ -0,0 +1,20 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueJsx from '@vitejs/plugin-vue-jsx'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  // eslint-disable-next-line no-undef
+  base: process.env.NODE_ENV === 'production' ? '/2023F2E_campaign_website/' : '/',
+  plugins: [
+    vue(),
+    vueJsx(),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
+})
