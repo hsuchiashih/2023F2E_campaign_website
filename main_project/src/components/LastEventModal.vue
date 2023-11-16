1619dccd2ac04d4c429900e34811446cdec6f029
hsuchiashih <hsuchiashih@gmail.com>
2023-11-16 17:48:41 +0000

    img

--- main_project/src/components/LastEventModal.vue
+++ main_project/src/components/LastEventModal.vue
@@ -5,21 +5,21 @@ const eventList = ref([
   {
     id: '1',
     title:'參與台北寵物論壇，爭取貓咪友善環境',
-    img: '/src/assets/img/lastEvents/main_event.svg',
+    img: '@/assets/img/lastEvents/main_event.svg',
     date: '2023/12/26',
     context: '炎炎夏日的周六，我走進了台北寵物論壇，帶著一副貓耳髮箍，決定要全力宣傳「貓咪至上」的理念！我相信，我們的都市中，每一隻貓咪都應該有自己的 VIP 休憩空間。'
   },
   {
     id: '2',
     title:'掃街模式開啟！帶著你的貓耳，來和我一起走！',
-    img: '/src/assets/img/lastEvents/event_01.svg',
+    img: '@/assets/img/lastEvents/event_01.svg',
     date: '2023/12/24',
     context: '街上氣氛真的很棒，從小孩到大人，甚至有些狗狗朋友都帶著貓耳來找我握手，真的太可愛了！'
   },
   {
     id: '3',
     title:'收容所模特兒大比拼！',
-    img: '/src/assets/img/lastEvents/event_02.svg',
+    img: '@/assets/img/lastEvents/event_02.svg',
     date: '2023/12/20',
     context: '今天的收容所不再是一片寂靜。為了讓更多人認識到這裡的毛孩子，我們舉辦了一場前所未有的「模特兒走秀」！'
   },
